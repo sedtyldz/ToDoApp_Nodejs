e7e84cfe37153503c9bf89d8982e60e3465f4d42
sedtyldz <ikinciyargic@gmail.com>
2025-03-15 15:34:51 +0300

    Fixing and Adding Html/css files

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require('express');// fronend kısmı için framework nesnesi oluşturma
-const bodyParser = require('body-parser'); // html sayfasından gelen parametrelere ulaşmak ve kullanmak için gerekli
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const my_app = express();
 
-
 my_app.set('view engine', 'ejs');
-my_app.set('views', __dirname + '/View');
+my_app.set('views', __dirname + "/views");
 
 
-my_app.get("/",(req,res)=>{
+my_app.use(express.static(__dirname + "/public"));
+
+my_app.get("/", (req, res) => {
     res.render('main');
-})
+});
 
-
-// server 3000 portunda çalışıyor
 my_app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
 });
